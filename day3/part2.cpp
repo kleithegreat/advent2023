139a1b775be4a232f2478ab0b9ba458142d40aa9
Kevin Lei <kevvinlei89@gmail.com>
2023-12-06 22:24:34 -0600

    day 3 part 2 attempt

--- day3/part2.cpp
+++ day3/part2.cpp
@@ -27,6 +27,7 @@ int main() {
     input.close();
 
     // identify start and end indicies of numbers
+    //im gay, suck my dick
     struct number {
         int x0;
         int y0;
@@ -55,8 +56,7 @@ int main() {
         }
     }
 
-    // search for symbols
-    string symbols = "!@#$%^&*()_+{}|:\"<>?`-=[]\\;',/~";
+    // search for gear ratios
     for (int i = 0; i < numbers.size(); i++) {
         int x0 = numbers[i].x0;
         int y0 = numbers[i].y0;
@@ -65,23 +65,45 @@ int main() {
         bool found = false;
         for (int j = max(0, y0 - 1); j <= min(139, y0 + 1); j++) {
             for (int k = max(0, x0 - 1); k <= min(139, xf + 1); k++) {
-                if (symbols.find(map[j][k]) != string::npos) {
-                    found = true;
-                    break;
+                if (map[j][k] == '*') {
+                    for (int l = max(0, j - 1); l <= min(139, j + 1); l++) {
+                        if (l == j) {
+                            continue;
+                        }
+
+                        for (int m = max(0, k - 1); m <= min(139, k + 1); m++) {
+                            if (m == k) {
+                                continue;
+                            }
+
+                            if (isdigit(map[l][m])) {
+                                string num1 = "";
+                                for (int n = numbers[i].x0; n <= numbers[i].xf; n++) {
+                                    num1 += map[numbers[i].y0][n];
+                                }
+
+                                int o = m;
+                                while (isdigit(map[l][o])) {
+                                    o--;
+                                }
+                                o++;
+
+                                string num2 = "";
+                                for (int n = o; n <= m; n++) {
+                                    num2 += map[l][n];
+                                }
+
+                                sum += stoi(num1) * stoi(num2);
+                                std::cout << num1 << " * " << num2 << " = " << stoi(num1) * stoi(num2) << std::endl;
+                            }
+                        }
+                    }
                 }
             }
         }
-
-        if (found) {
-            string num = "";
-            for (int j = x0; j <= xf; j++) {
-                num += map[y0][j];
-            }
-            sum += stoi(num);
-        }
     }
 
-    // deallocate 2D array
+    // deallocate 2D array cum in mt mouth
     for (int i = 0; i < 140; i++) {
         delete [] map[i];
     }
