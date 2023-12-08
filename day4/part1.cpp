a7ae036398d0b1de56dd670f85a59f6ec787fb57
kevin <kevvinlei89@gmail.com>
2023-12-07 19:38:40 -0600

    day 4 part 1 done

---
+++ day4/part1.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using std::string;
+using std::vector;
+using std::cout;
+using std::endl;
+
+int main() {
+    // read file
+    vector<string> input;
+    string line;
+    std::ifstream file("input.txt");
+
+    while (getline(file, line)) {
+        input.push_back(line);
+    }
+    file.close();
+
+    // parse input
+    struct Card {
+        vector<int> winners;
+        vector<int> given;
+    };
+
+    vector<Card> cards;
+
+    for (int i = 0; i < input.size(); i++) {
+        int COLON_POS = 8;
+        int PIPE_POS = 40;
+
+        string winners = input[i].substr(COLON_POS + 1, PIPE_POS);
+        string given = input[i].substr(PIPE_POS + 1);
+
+        // read into vectors
+        Card card;
+        std::stringstream ss(winners);
+        int buffer;
+        while (ss >> buffer) {
+            card.winners.push_back(buffer);
+        }
+        ss.clear();
+        ss.str(given);
+        while (ss >> buffer) {
+            card.given.push_back(buffer);
+        }
+
+        cards.push_back(card);
+    }
+
+    // determine winnings
+    int totalPoints = 0;
+
+    for (int i = 0; i < cards.size(); i++) {
+        int matchingCards = 0;
+        for (int j = 0; j < cards[i].winners.size(); j++) {
+            if (std::find(cards[i].given.begin(), cards[i].given.end(), cards[i].winners[j]) != cards[i].given.end()) {
+                matchingCards++;
+            }
+        }
+
+        int points = pow(2, matchingCards - 1);
+        totalPoints += points;
+    }
+
+    cout << "Total points: " << totalPoints << endl;
+
+    return 0;
+}
